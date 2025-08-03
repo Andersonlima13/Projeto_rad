07552a0770a55cab4aee986b587a31eb6f188c6e
Andersonlima13 <anderson.lima18pb@gmail.com>
2025-08-03 11:27:49 -0300

    fluxo de autenticacao funcionando

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,13 @@
+# app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
-  def destroy
-    super
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  rescue => e
+    flash.now[:alert] = "Falha no login: #{e.message}"
+    render :new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,14 @@ Rails.application.routes.draw do
     sessions: "users/sessions"  # Adicione esta linha
   }
 
-  # Rota alternativa para logout via GET (apenas para desenvolvimento)
-  devise_scope :user do
-    get "/users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session_get
+   authenticated :user do
+    root "dashboard#index", as: :dashboard
   end
+  unauthenticated do
+    root "home#index"
+  end
+
+
 
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "home#index"
